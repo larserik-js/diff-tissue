552925f25e3bb009d194ccc5d85ca1f0dd5cdf21
Lars Erik J. Skjegstad <lars_erik_skjegstad@hotmail.com>
2025-03-28 13:34:55 +0100

    feat: better init conditions for optimization

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ import optax
 import init_systems
 
 
-_N_SHAPE_STEPS = 100
+_N_SHAPE_STEPS = 1000
 _N_GROWTH_STEPS = 500
 _LEARNING_RATE = 1e-4
 
 _AREAS_LOSS_WEIGHT = 10.0
-_ANGLES_LOSS_WEIGHT = 5.0
+_ANGLES_LOSS_WEIGHT = 2.0
 _ASPECT_RATIO_LOSS_WEIGHT = 100.0
 _OPTIMAL_ASPECT_RATIO = 1/8
 
@@ -273,7 +273,7 @@ def _iterate_over_growth(goal_areas, jax_arrays):
 
 
 def _sigmoid(variations):
-    return 1.0 + 2.0 * jax.nn.sigmoid(variations)
+    return 1.0 + 9.0 * jax.nn.sigmoid(variations)
 
 
 def _make_ellipse(num_points=50, a=10.0, b=15.0, center=(40.0, 40.0)):
@@ -340,7 +340,7 @@ def _iterate_towards_shape(jax_arrays):
 
     val_grad_loss = jax.jit(jax.value_and_grad(shape_loss_func, has_aux=True))
 
-    variations = jnp.zeros_like(init_areas)
+    variations = jnp.zeros_like(init_areas) + 4.0
 
     fig, ax = plt.subplots(figsize=(10, 10))
     ax_lims = _get_ax_lims(init_vertices)
