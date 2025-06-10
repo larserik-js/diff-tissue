fix: correct aspect ratio scales calculation
Previously it was assumed that an aspect ratio of 1.0 equals a square,
but our calculations use width / (width + height), which gives an aspect
ratio of 0.5.

growth.py
@@ -202,13 +202,13 @@ def _main():
 
     aspect_ratio_scales = np.where(
         np.isclose(polygons.get_basal_mask(), 0), 1.0,
-        polygons.get_basal_mask() / params.numerical['optimal_aspect_ratio']
+        (polygons.get_basal_mask() *
+         (1 / params.numerical['optimal_aspect_ratio'] - 1.0))
     )
     goal_areas = (
         params.numerical['max_area_scaling'] * init_areas * aspect_ratio_scales
     )
     goal_aspect_ratios = 0.5 * jnp.ones_like(init_areas)
-
 
     output_dir = my_utils.OutputDir('growth', params)
     output_dir.make()

main.py
@@ -14,7 +14,9 @@ def _sigmoid(max_area_scaling, variations):
 def _calc_aspect_ratio_scales(jax_arrays, optimal_aspect_ratio):
     basal_mask = jax_arrays['basal_mask']
     aspect_ratio_scales = np.ones(len(basal_mask))
-    aspect_ratio_scales[basal_mask] /= optimal_aspect_ratio
+    aspect_ratio_scales[basal_mask] = (
+        aspect_ratio_scales[basal_mask] * (1 / optimal_aspect_ratio - 1.0)
+    )
     return jnp.array(aspect_ratio_scales)
 
 