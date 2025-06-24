ba108827e50513be8405f77422ead06673e8832c
Lars Erik J. Skjegstad <lars_erik_skjegstad@hotmail.com>
2025-06-24 11:05:41 +0200

    feat: add aspect ratio to learned_growth.py

--- learned_growth.py
+++ learned_growth.py
@@ -2,8 +2,6 @@ import jax.numpy as jnp
 import numpy as np
 import pandas as pd
 from shapely.geometry import Polygon
-from shapely.ops import unary_union
-
 
 import growth, my_utils
 
@@ -19,7 +17,7 @@ def _make_poly_idx_lists(jax_arrays):
     return poly_idx_lists
 
 
-def build_polygons(jax_arrays):
+def _build_polygons(jax_arrays):
     poly_idx_lists = _make_poly_idx_lists(jax_arrays)
     polygons = []
     for idx_list in poly_idx_lists:
@@ -32,43 +30,40 @@ def build_polygons(jax_arrays):
     return polygons
 
 
-def assign_weighted_goal_areas(old_polygons, goal_areas, new_polygons):
-    new_goal_areas = []
+def _assign_weighted_goals(old_polygons, goals, new_polygons):
+    new_goals = []
 
     for new_poly in new_polygons:
         intersections = []
         goal_parts = []
-        goal_areas_ = []
+        goals_ = []
 
-        for old_poly, goal_area in zip(old_polygons, goal_areas):
+        for old_poly, goal in zip(old_polygons, goals):
             inter = new_poly.intersection(old_poly)
             if not inter.is_empty:
                 inter_area = inter.area
                 intersections.append(inter_area)
-                goal_parts.append((inter_area, goal_area))
-                goal_areas_.append(goal_area)
+                goal_parts.append((inter_area, goal))
+                goals_.append(goal)
         
         intersections = np.array(intersections)
-        goal_areas_ = np.array(goal_areas_)
+        goals_ = np.array(goals_)
         total_inter_area = intersections.sum()
 
         if np.isclose(total_inter_area, 0.0):
-            new_goal_areas.append(0.0)
+            new_goals.append(0.0)
         else:
-            # weighted_sum = sum((inter_area / total_inter_area) * goal_area
-            #                    for inter_area, goal_area in goal_parts)
-            weighted_goal_area = np.sum(
-                intersections * goal_areas_ / total_inter_area
+            weighted_goal = np.sum(
+                intersections * goals_ / total_inter_area
             )
-            # new_goal_areas.append(weighted_sum)
-            new_goal_areas.append(weighted_goal_area)
+            new_goals.append(weighted_goal)
     
-    new_goal_areas = np.array(new_goal_areas)
+    new_goals = jnp.array(new_goals)
 
-    return new_goal_areas
+    return new_goals
 
 
-def main():
+def _main():
     params = my_utils.Params()
 
     np.random.seed(params.numerical['seed'])
@@ -77,33 +72,33 @@ def main():
     learned_growth_dir.make()
 
     arrays = my_utils.get_arrays(params)
-    old_polygons = build_polygons(arrays)
+    old_polygons = _build_polygons(arrays)
 
     input_file = my_utils.get_output_params_file(params)
     df = pd.read_csv(input_file, sep='\t', index_col=0)
     
     goal_areas = df['goal_area'].values
-    # goal_aspect_ratios = df['goal_aspect_ratio'].values
+    goal_aspect_ratios = df['goal_aspect_ratio'].values
 
     # Regenerate new system
-    np.random.seed(10001)
+    np.random.seed(10000)
     new_arrays = my_utils.get_arrays(params)
-    new_polygons = build_polygons(new_arrays)
+    new_polygons = _build_polygons(new_arrays)
 
-    resulting_areas = assign_weighted_goal_areas(
+    resulting_areas = _assign_weighted_goals(
         old_polygons, goal_areas, new_polygons
     )
+    resulting_aspect_ratios = _assign_weighted_goals(
+        old_polygons, goal_aspect_ratios, new_polygons
+    )
 
-    resulting_areas = jnp.array(resulting_areas)
-    mock_aspect_ratios = 0.5 * jnp.ones_like(resulting_areas)
-    
     jax_arrays = my_utils._make_jax_arrays(new_arrays)
 
     growth.iterate_and_plot(
         learned_growth_dir.get_param_path(), resulting_areas,
-        mock_aspect_ratios, jax_arrays, params.numerical
+        resulting_aspect_ratios, jax_arrays, params.numerical
     )
 
 
 if __name__ == '__main__':
-    main()
+    _main()
