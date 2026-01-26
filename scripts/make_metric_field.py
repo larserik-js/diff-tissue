5d370987e5283439d972e55065cdafd240454e0a
Lars Erik J. Skjegstad <lars_erik_skjegstad@hotmail.com>
2026-01-26 09:46:25 +0100

    feat: split make_metric_field.py
    Add module plot_mapped_metrics.py and move plotting functionality there
    from make_metric_field.py.

--- scripts/make_metric_field.py
+++ scripts/make_metric_field.py
@@ -3,8 +3,6 @@ import pathlib
 import pickle
 
 import jax
-from matplotlib import colors
-import matplotlib.pyplot as plt
 import numpy as np
 import shapely
 from shapely.strtree import STRtree
@@ -139,58 +137,11 @@ def _save_fields(grid_coords, area_field_grid, elongation_field_grid,
         pickle.dump(output, f)
 
 
-def _add_colorbar(ax, cmap_vals, cmap_name):
-    normalize = colors.Normalize(
-        vmin = np.nanmin(cmap_vals),
-        vmax = np.nanmax(cmap_vals)
-    )
-    cmap = plt.get_cmap(cmap_name)
-    sm = plt.cm.ScalarMappable(norm=normalize, cmap=cmap)
-    sm.set_array(cmap_vals)
-    ax.figure.colorbar(sm, ax=ax, shrink=0.8)
-
-
-def _plot(grid_coords, area_field_grid, elongation_field_grid):
-    xmin, ymin = grid_coords.min(axis=0)
-    xmax, ymax = grid_coords.max(axis=0)
-
-    field_grids = [area_field_grid, elongation_field_grid]
-
-    titles = ['Goal areas', 'Goal elongations']
-    cmaps = ['copper', 'viridis']
-
-    fig, axs = plt.subplots(2, figsize=(5,6))
-    for i, ax in enumerate(axs):
-        field_grid = field_grids[i]
-        ax.imshow(
-            field_grid, cmap=cmaps[i], extent=(xmin, xmax, ymin, ymax),
-            origin='lower', aspect='auto',
-        )
-        _add_colorbar(ax, field_grid, cmaps[i])
-        ax.set_title(titles[i])
-        ax.set_aspect('equal')
-        ax.set_xlim(-11.0, 11.0)
-        ax.set_ylim(-0.5, 16.1)
-        if i == 0:
-            ax.set_xticklabels([])
-        if i == 1:
-            ax.set_xlabel('$x$')
-
-    fig.tight_layout()
-    return fig
-
-
 def _to_grid(field, grid_coords, mask, nx, ny):
     field_full = np.full(len(grid_coords), np.nan)
     field_full[mask] = field
     field_grid = field_full.reshape((ny, nx))
     return field_grid
-
-
-def _save_plot(fig):
-    output_file = pathlib.Path('outputs/fields.pdf')
-    output_file.parent.mkdir(exist_ok=True)
-    fig.savefig(output_file)
 
 
 def _main():
@@ -225,10 +176,6 @@ def _main():
     _save_fields(
         grid_coords, area_field_grid, elongation_field_grid, fields_file
     )
-
-    fig = _plot(grid_coords, area_field_grid, elongation_field_grid)
-
-    _save_plot(fig)
 
 
 if __name__ == '__main__':
