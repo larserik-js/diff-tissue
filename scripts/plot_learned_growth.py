fa9722dfb34cd3de51b5c8e89ebdb4c7c74aa943
Lars Erik J. Skjegstad <lars_erik_skjegstad@hotmail.com>
2026-02-20 14:02:57 +0100

    refactor(learned growth): make experiment fn

--- scripts/plot_learned_growth.py
+++ scripts/plot_learned_growth.py
@@ -1,17 +1,10 @@
-from diff_tissue.app import io_utils, learned_growth, parameters
+from diff_tissue.app import experiments, parameters
 
 
 def _main():
     params = parameters.get_params_from_cli()
 
-    results = learned_growth.run(params)
-
-    output = io_utils.OutputManager(
-        learned_growth.OUTPUT_TYPE_DIR, base_dir="outputs"
-    )
-
-    param_string = parameters.get_param_string(params)
-    learned_growth.plot(results, output, param_string)
+    experiments.run_learned_growth(params)
 
 
 if __name__ == "__main__":
--- src/diff_tissue/app/experiments.py
+++ src/diff_tissue/app/experiments.py
@@ -1,9 +1,12 @@
 from ..core import my_utils
-from . import io_utils, morphing, parameters
+from . import io_utils, learned_growth, morphing, parameters
 from . import shape_opt as shape_opt_app
 
 
-def run_morphing(params, base_dir="outputs"):
+OUTPUT_DIR = "outputs"
+
+
+def run_morphing(params, base_dir=OUTPUT_DIR):
     jax_arrays = my_utils.get_jax_arrays(params)
 
     output = io_utils.OutputManager(
@@ -22,7 +25,7 @@ def run_morphing(params, base_dir="outputs"):
     )
 
 
-def run_shape_opt(params, base_dir="outputs"):
+def run_shape_opt(params, base_dir=OUTPUT_DIR):
     shape_opt_app.optimize_shape(params, base_dir=base_dir)
 
     best_goal_areas, best_goal_anisotropies = shape_opt_app.load_output_params(
@@ -43,3 +46,15 @@ def run_shape_opt(params, base_dir="outputs"):
     shape_opt_app.plot_best_growth(
         best_growth_evolution, output, param_string, jax_arrays, params
     )
+
+
+def run_learned_growth(params):
+    results = learned_growth.run(params)
+
+    output = io_utils.OutputManager(
+        learned_growth.OUTPUT_TYPE_DIR, base_dir=OUTPUT_DIR
+    )
+
+    param_string = parameters.get_param_string(params)
+
+    learned_growth.plot(results, output, param_string)
