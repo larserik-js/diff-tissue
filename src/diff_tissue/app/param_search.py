f049dc163a9e3ef7c09ef6c8e07b975cda48ce5a
Lars Erik J. Skjegstad <lars_erik_skjegstad@hotmail.com>
2026-04-17 10:17:36 +0200

    chore(param_search): replace searches.py

--- src/diff_tissue/app/param_search.py
+++ src/diff_tissue/app/param_search.py
@@ -66,3 +66,50 @@ def run(paths):
 
     print("Best loss:", study.best_value)
     print("Best params:", study.best_params)
+
+
+def _show_studies(storage):
+    study_summaries = optuna.get_all_study_summaries(storage)
+
+    for s in study_summaries:
+        print(
+            f"Study name: {s.study_name}, "
+            f"Direction: {s.direction}, "
+            f"Trials: {s.n_trials}, "
+            f"Best value: {s.best_trial.value if s.best_trial else None}"
+        )
+    print("")
+
+
+def _show_first_trials(study):
+    df = study.trials_dataframe()
+    print(df.head())
+    print("")
+
+
+def _show_best_trial(study):
+    print(f"{'Best loss:':<30}{study.best_value}")
+    print("")
+
+    for param, val in study.best_params.items():
+        print(f"{param + ':':<30}{val}")
+    print("")
+
+
+def _show_n_completed(study):
+    trials = study.get_trials(deepcopy=False)
+    n_completed = sum(1 for t in trials if t.state.is_finished())
+    print(f"Trials completed: {n_completed}")
+
+
+def inspect_param_search(paths, study_name):
+    param_search_paths = ParamSearchPaths(paths)
+    storage = optuna.storages.RDBStorage(param_search_paths.db_url)
+
+    _show_studies(storage)
+
+    study = optuna.load_study(study_name=f"{study_name}", storage=storage)
+
+    _show_first_trials(study)
+    _show_best_trial(study)
+    _show_n_completed(study)
